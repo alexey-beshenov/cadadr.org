a86d66fd4e0902b7d59a0c3dcb9c999a880cd2ce
Alexey Beshenov <cadadr@gmail.com>
2022-01-05 02:20:11 +0300

    Highlight current page

--- site.hs
+++ site.hs
@@ -21,15 +21,35 @@ main = hakyll $ do
         compile $ pandocCompiler_
             >>= loadAndApplyTemplate "templates/default.html" mainCtx
 
-    match ("links.md" .||. "misc.md" .||. "talks/*.md" .||. "papers/*.md") $ do
+    match ("links.md") $ do
         route   $ setExtension "html"
         compile $ pandocCompiler_
             >>= loadAndApplyTemplate "templates/default.html" postCtx
+
+    match ("misc.md") $ do
+        route   $ setExtension "html"
+        compile $ pandocCompiler_
+            >>= loadAndApplyTemplate "templates/default.html" miscCtx
+
+    match ("talks/index.md") $ do
+        route   $ setExtension "html"
+        compile $ pandocCompiler_
+            >>= loadAndApplyTemplate "templates/default.html" talksCtx
+
+    match ("papers/index.md") $ do
+        route   $ setExtension "html"
+        compile $ pandocCompiler_
+            >>= loadAndApplyTemplate "templates/default.html" papersCtx
 
     match "teaching/cimat-tna/*.md" $ do
         route   $ setExtension "html"
         compile $ pandocCompiler_
             >>= loadAndApplyTemplate "templates/default.html" teachingTNACtx
+
+    match "teaching/index.md" $ do
+        route   $ setExtension "html"
+        compile $ pandocCompiler_
+            >>= loadAndApplyTemplate "templates/default.html" teachingMainCtx
 
     match "teaching/**.md" $ do
         route   $ setExtension "html"
@@ -54,9 +74,33 @@ postCtx =
     defaultContext
 
 --------------------------------------------------------------------------------
+papersCtx :: Context String
+papersCtx =
+    constField "papers" "true" `mappend`
+    postCtx
+
+--------------------------------------------------------------------------------
+talksCtx :: Context String
+talksCtx =
+    constField "talks" "true" `mappend`
+    postCtx
+
+--------------------------------------------------------------------------------
+teachingMainCtx :: Context String
+teachingMainCtx =
+    constField "teachingMain" "true" `mappend`
+    teachingCtx
+
+--------------------------------------------------------------------------------
 teachingCtx :: Context String
 teachingCtx =
     constField "teaching" "true" `mappend`
+    postCtx
+
+--------------------------------------------------------------------------------
+miscCtx :: Context String
+miscCtx =
+    constField "misc" "true" `mappend`
     postCtx
 
 --------------------------------------------------------------------------------
